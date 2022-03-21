975fb00290ec68c79817ecf33251b2967b56efcd
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-21 12:34:20 +0530

    Add files via upload

---
+++ Python_Topic3_Assingment_Operators.py
@@ -0,0 +1,31 @@
+# Perform All Assingment Operators
+a = 20   # Assigning 20 to the variable a
+
+
+
+# Using  +=  Compond Assingment Operator
+b+=5
+print("b+=5 --->",b);
+
+# Using   -=  Compond Assingment Operator
+
+
+
+# Using  *=  Compond Assingment Operator
+
+
+
+# Using   /=  Compond Assingment Operator
+
+
+
+# Using   %=  Compond Assingment Operator
+
+
+
+# Using  //=  Compond Assingment Operator
+
+
+
+#   **=  Compond Assingment Operator
+
